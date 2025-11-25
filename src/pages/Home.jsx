4ad7e7d9e0ce9e2d35f2aee6790b440be5aa1f60
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2025-11-25 14:50:41 +0600

    fact section updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,7 @@ import DevelopmentProject from "../components/home/DevelopmentProject.component"
 import Hero from "../components/home/Hero.component";
 import HomeVideo from "../components/home/HomeVideo.component";
 import Stats from "../components/home/Stats.section";
-import FactsSection from "../components/home/Test";
-import Test from "../components/home/Test";
+import FactsSection from "../components/home/FactsSection";
 import ClientFeedback from "../components/home/testimonial/Testimonial";
 import BeamCircle from "../components/ui/BeamCircle";
 
